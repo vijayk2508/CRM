379bf7f5c8993ad2560f4b07cb64a086a20db047
Vijay Kumar <asonv.tech@gmail.com>
2023-05-27 23:32:26 +0530

    update

--- src/components/pages/Customer/CreateEditForm.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CreateEditForm: React.FC = () => {
-  return <div></div>;
-};
-
-export default CreateEditForm;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppRoutes from './AppRoutes/AppRoutes';
-import './moke-api'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./AppRoutes/AppRoutes";
+import "./moke-api";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <AppRoutes />
